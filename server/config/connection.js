90d573efc8b00c1cc29cc73caaf4d00501f9a2e9
malikdreamy <119153047+malikdreamy@users.noreply.github.com>
2023-05-24 18:19:51 -0700

    Delete server directory

--- server/config/connection.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-
-// mongoose.set('strictQuery', true)
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Algorithms');
-
-module.exports = mongoose.connection;
